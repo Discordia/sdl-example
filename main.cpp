c91759679d5c56a199f38d258321d00f5839f816
Robert Sjödahl <robert@mojang.com>
2017-02-26 19:37:39 +0100

    First commit

---
+++ main.cpp
@@ -0,0 +1,243 @@
+// Headerphile.com OpenGL Tutorial part 1
+// A Hello World in the world of OpenGL ( creating a simple windonw and setting background color )
+// Source code is an C++ adaption / simplicication of : https://www.opengl.org/wiki/Tutorial1:_Creating_a_Cross_Platform_OpenGL_3.2_Context_in_SDL_(C_/_SDL)
+// Compile : clang++ main.cpp -lGL -lSDL2 -std=c++11 -o Test
+
+#include <string>
+#include <iostream>
+#include "glad.h"
+#include "linmath.h"
+#include <SDL2/SDL.h>
+
+std::string programName = "SDL2 - OpenGL";
+
+static const struct {
+    float x, y;
+    float r, g, b;
+} vertices[3] = {
+        {-0.6f, -0.4f, 1.f, 0.f, 0.f},
+        {0.6f,  -0.4f, 0.f, 1.f, 0.f},
+        {0.f,   0.6f,  0.f, 0.f, 1.f}
+};
+
+static const char *vertex_shader_text =
+        "uniform mat4 MVP;\n"
+                "attribute vec3 vCol;\n"
+                "attribute vec2 vPos;\n"
+                "varying vec3 color;\n"
+                "void main()\n"
+                "{\n"
+                "    gl_Position = MVP * vec4(vPos, 0.0, 1.0);\n"
+                "    color = vCol;\n"
+                "}\n";
+
+static const char *fragment_shader_text =
+        "varying vec3 color;\n"
+                "void main()\n"
+                "{\n"
+                "    gl_FragColor = vec4(color, 1.0);\n"
+                "}\n";
+
+
+// Our SDL_Window ( just like with SDL2 wihout OpenGL)
+SDL_Window *mainWindow;
+
+// Our opengl context handle
+SDL_GLContext mainContext;
+
+bool SetOpenGLAttributes();
+void PrintSDL_GL_Attributes();
+void CheckSDLError(int line);
+void RunGame();
+void Cleanup();
+
+
+GLuint vertex_buffer, vertex_shader, fragment_shader, program;
+GLint mvp_location, vpos_location, vcol_location;
+GLint w, h;
+
+bool Init()
+{
+    // Initialize SDL's Video subsystem
+    if (SDL_Init(SDL_INIT_VIDEO) < 0)
+    {
+        std::cout << "Failed to init SDL\n";
+        return false;
+    }
+
+    // Create our window centered at 512x512 resolution
+    mainWindow = SDL_CreateWindow(programName.c_str(),
+                                  SDL_WINDOWPOS_CENTERED,
+                                  SDL_WINDOWPOS_CENTERED,
+                                  640, 480,
+                                  SDL_WINDOW_OPENGL);
+
+    // Check that everything worked out okay
+    if (!mainWindow)
+    {
+        std::cout << "Unable to create window\n";
+        CheckSDLError(__LINE__);
+        return false;
+    }
+
+    // Create our opengl context and attach it to our window
+    // SetOpenGLAttributes();
+    mainContext = SDL_GL_CreateContext(mainWindow);
+
+
+    // This makes our buffer swap syncronized with the monitor's vertical refresh
+    SDL_GL_SetSwapInterval(1);
+
+    // Init GLAD
+    // Check OpenGL properties
+    printf("OpenGL loaded\n");
+    gladLoadGLLoader(SDL_GL_GetProcAddress);
+    printf("Vendor:   %s\n", glGetString(GL_VENDOR));
+    printf("Renderer: %s\n", glGetString(GL_RENDERER));
+    printf("Version:  %s\n", glGetString(GL_VERSION));
+
+    SDL_GL_GetDrawableSize(mainWindow, &w, &h);
+    std::cout << "Width: " << w << ", height: " << h << std::endl;
+
+    return true;
+}
+
+bool SetOpenGLAttributes()
+{
+    // Set our OpenGL version.
+    // SDL_GL_CONTEXT_CORE gives us only the newer version, deprecated functions are disabled
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
+
+    // 3.2 is part of the modern versions of OpenGL, but most video cards would be able to run it
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 2);
+
+    // Turn on double buffering with a 24bit Z buffer.
+    // You may need to change this to 16 or 32 for your system
+    SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
+
+    return true;
+}
+
+int main(int argc, char *argv[])
+{
+    if (!Init())
+        return -1;
+
+    glGenBuffers(1, &vertex_buffer);
+    glBindBuffer(GL_ARRAY_BUFFER, vertex_buffer);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    vertex_shader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertex_shader, 1, &vertex_shader_text, NULL);
+    glCompileShader(vertex_shader);
+
+    fragment_shader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragment_shader, 1, &fragment_shader_text, NULL);
+    glCompileShader(fragment_shader);
+
+    program = glCreateProgram();
+    glAttachShader(program, vertex_shader);
+    glAttachShader(program, fragment_shader);
+    glLinkProgram(program);
+
+    mvp_location = glGetUniformLocation(program, "MVP");
+    vpos_location = glGetAttribLocation(program, "vPos");
+    vcol_location = glGetAttribLocation(program, "vCol");
+    glEnableVertexAttribArray(vpos_location);
+    glVertexAttribPointer(vpos_location, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 5, (void *) 0);
+
+    glEnableVertexAttribArray(vcol_location);
+    glVertexAttribPointer(vcol_location, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 5, (void *) (sizeof(float) * 2));
+
+    // Clear our buffer with a black background
+    // This is the same as :
+    // 		SDL_SetRenderDrawColor(&renderer, 255, 0, 0, 255);
+    // 		SDL_RenderClear(&renderer);
+    //
+    glClearColor(0.0, 0.0, 0.0, 1.0);
+    glClear(GL_COLOR_BUFFER_BIT);
+    SDL_GL_SwapWindow(mainWindow);
+
+    RunGame();
+
+    Cleanup();
+
+    return 0;
+}
+
+void RunGame()
+{
+    bool loop = true;
+
+    while (loop)
+    {
+        SDL_Event event;
+        while (SDL_PollEvent(&event))
+        {
+            if (event.type == SDL_QUIT)
+                loop = false;
+
+            if (event.type == SDL_KEYDOWN)
+            {
+                switch (event.key.keysym.sym)
+                {
+                    case SDLK_ESCAPE:
+                        loop = false;
+                        break;
+                }
+            }
+        }
+
+        float ratio;
+        mat4x4 m, p, mvp;
+        ratio = w / (float) h;
+        glViewport(0, 0, w, h);
+        glClear(GL_COLOR_BUFFER_BIT);
+        mat4x4_identity(m);
+        mat4x4_rotate_Z(m, m, (float) SDL_GetTicks() / 1000.0f);
+        mat4x4_ortho(p, -ratio, ratio, -1.f, 1.f, 1.f, -1.f);
+        mat4x4_mul(mvp, p, m);
+        glUseProgram(program);
+        glUniformMatrix4fv(mvp_location, 1, GL_FALSE, (const GLfloat *) mvp);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+        SDL_GL_SwapWindow(mainWindow);
+    }
+}
+
+void Cleanup()
+{
+    // Delete our OpengL context
+    SDL_GL_DeleteContext(mainContext);
+
+    // Destroy our window
+    SDL_DestroyWindow(mainWindow);
+
+    // Shutdown SDL 2
+    SDL_Quit();
+}
+
+void CheckSDLError(int line = -1)
+{
+    std::string error = SDL_GetError();
+
+    if (error != "")
+    {
+        std::cout << "SLD Error : " << error << std::endl;
+
+        if (line != -1)
+            std::cout << "\nLine : " << line << std::endl;
+
+        SDL_ClearError();
+    }
+}
+
+void PrintSDL_GL_Attributes()
+{
+    int value = 0;
+    SDL_GL_GetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, &value);
+    std::cout << "SDL_GL_CONTEXT_MAJOR_VERSION : " << value << std::endl;
+
+    SDL_GL_GetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, &value);
+    std::cout << "SDL_GL_CONTEXT_MINOR_VERSION: " << value << std::endl;
+}
